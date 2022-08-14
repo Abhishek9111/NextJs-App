7e96eed2c5cfb4cecff29f6073581fb245380a8b
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2022-08-14 20:56:28 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    MONGO_URI:
+      "mongodb+srv://abhishek9111:4l2voZ94qgIm4i3l@cluster0.pmtlxq2.mongodb.net/?retryWrites=true&w=majority",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
